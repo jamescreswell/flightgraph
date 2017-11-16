22b2a15f468d9a782a07d54594c03bc0c3f3c09d
jamescreswell <jamescreswell4@gmail.com>
2017-11-16 21:09:29 +0100

    Add airport model

--- flightgraph/settings.py
+++ flightgraph/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '54.91.110.22']
 # Application definition
 
 INSTALLED_APPS = [
+    'flights.apps.FlightsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Airport(models.Model):
+    iata = models.CharField(max_length=3, blank=True)
+    icao = models.CharField(max_length=4, blank=True)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    elevation = models.FloatField()
+    opened = models.DateField(blank=True, null=True, help_text='YYYY-MM-DD')
+    closed = models.DateField(blank=True, null=True, help_text='YYYY-MM-DD')
+    
+    def __str__(self):
+        if self.iata is not '':
+            return self.iata
+        elif self.icao is not '':
+            return self.icao
+        else:
+            return self.name[:4] + '...'
