2df2b53898cf7a1fe41a906e27b4bac34e006ac4
jamescreswell <jamescreswell4@gmail.com>
2017-11-16 20:51:38 +0100

    Change database to sqlite3

--- flightgraph/settings.py
+++ flightgraph/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'flightgraph.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'flightgraph',
-        'USER': 'root',
-        'PASSWORD': 'bear',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'flightgraph',
+    #    'USER': 'root',
+    #    'PASSWORD': 'bear',
+    #    'HOST': '',
+    #    'PORT': '',
+    #}
 }
 
 
