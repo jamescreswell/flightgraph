c80612dab77bf4174de1b0016efc2a390af54890
jamescreswell <jamescreswell4@gmail.com>
2017-11-16 21:12:40 +0100

    Test airport model

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Airport
 
 def index(request):
-    return HttpResponse("Hello, world !")
+    string = ''
+    for airport in Airport.objects.all():
+        string += airport.name + ' (' + airport.iata + ')\n'
+    return HttpResponse(string)
