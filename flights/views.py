7f0aed90fd30f5a7a3b29bd29aa53a893f3fb71f
jamescreswell <jamescreswell4@gmail.com>
2017-11-17 22:56:20 +0100

    Create gcmap view

--- flights/views.py
+++ flights/views.py
@@ -6,3 +6,14 @@ def index(request):
     context = {'title': 'Test',
               }
     return render(request, 'flights/index.html', context)
+
+def gcmap(request):
+    if request.method == 'POST':
+        print(request.POST['origin'])
+        origin = Airport.objects.filter(iata=request.POST['origin'])[0]
+        context = {'origin': origin}
+        return render(request, 'flights/gcmap.html', context)
+    else:
+        return HttpResponse('Missing POST')
+        #return render(request, 'flights/gcmap.html', context)
+    
