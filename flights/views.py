7e9cd721c60f75f9c5742fb1a3415be0598a3581
jamescreswell <jamescreswell4@gmail.com>
2017-11-21 22:42:10 +0100

    Route mapper

--- flights/views.py
+++ flights/views.py
@@ -10,18 +10,56 @@ def index(request):
 
 def gcmap(request):
     if request.method == 'POST':
-        print(request.POST['origin'])
-        origin = Airport.objects.filter(iata=request.POST['origin'])[0]
+        input_string = request.POST['input_string']
+        # input_string is of the format "abc-defg-hij-kil-...-xyz"
+        
+        # Extract codes
+        hyphen_indices = [i for i, ltr in enumerate(input_string) if ltr == '-']
+        codes = [input_string[0:hyphen_indices[0]],]
+        i = -1
+        for i in range(len(hyphen_indices)-1):
+            codes.append(input_string[hyphen_indices[i]+1:hyphen_indices[i+1]])
+        codes.append(input_string[hyphen_indices[i+1]+1:])
+        
+        # Look up codes
+        airports = []
+        for code in codes:
+            if len(code) == 3:
+                match = Airport.objects.filter(iata=code.upper())
+                if len(match) == 1:
+                    airports.append(match[0])
+                elif len(match) == 0:
+                    pass
+                else:
+                    # Problems...
+                    airports.append(match[0])
+            elif len(code) == 4:
+                match = Airport.objects.filter(icao=code.upper())
+                if len(match) == 1:
+                    airports.append(match[0])
+                elif len(match) == 0:
+                    pass
+                else:
+                    # Problems...
+                    airports.append(match[0])
+            else:
+                # This should never happen
+                print('Error')
+        print(airports)
+        
+        context = {'method': 'POST',
+                   'nav_id': 'gcmap_nav',
+                   'airports': airports,
+                  }
+        return render(request, 'flights/gcmap.html', context)
     else:
         origin = Airport.objects.filter(iata='DFW')[0]
-        #return HttpResponse('Missing POST')
-        #return render(request, 'flights/gcmap.html', context)
         
-    context = {'nav_id': 'gcmap_nav',
-               'origin': origin,
-               'distance': origin.distance_to(origin),
-              }
-    return render(request, 'flights/gcmap.html', context)
+        context = {'method': 'GET'}
+        
+        return render(request, 'flights/gcmap.html', context)
+        
+
 
 def airports(request):
     pass
