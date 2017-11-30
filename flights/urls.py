536227ee201bc24ed77c72cb701fa166eb597fc2
jamescreswell <jamescreswell4@gmail.com>
2017-11-30 22:50:12 +0100

    Implement AJAX gcmap

--- flights/urls.py
+++ flights/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^gcmap/$', views.gcmap, name='gcmap'),
     url(r'^airports/$', views.airports, name='airports'),
+    url(r'^ajax/draw_gcmap/$', views.draw_gcmap, name='draw_gcmap'),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden
+from django.core import serializers
 from .models import Airport
 
 def index(request):
@@ -9,70 +10,58 @@ def index(request):
     return render(request, 'flights/index.html', context)
 
 def gcmap(request):
-    if request.method == 'POST':
-        input_string = request.POST['input_string']
-        # input_string is of the format "abc-defg-hij-kil-...-xyz"
+    context = {'method': 'GET',
+               'nav_id': 'gcmap_nav',
+              }
+
+    return render(request, 'flights/gcmap.html', context)
         
-        # Extract codes
+def draw_gcmap(request):
+    if not request.is_ajax():
+        return HttpResponseForbidden('Permission denied: <tt>request.is_ajax() == False</tt>')
+    
+    input_string = request.GET.get('input_string')
+    # input_string is of the format "abc-defg-hij-kil-...-xyz"
+
+    # Extract codes
+    try:
         hyphen_indices = [i for i, ltr in enumerate(input_string) if ltr == '-']
         codes = [input_string[0:hyphen_indices[0]],]
         i = -1
         for i in range(len(hyphen_indices)-1):
             codes.append(input_string[hyphen_indices[i]+1:hyphen_indices[i+1]])
         codes.append(input_string[hyphen_indices[i+1]+1:])
-        
-        # Look up codes
-        airports = []
-        for code in codes:
-            if len(code) == 3:
-                match = Airport.objects.filter(iata=code.upper())
-                if len(match) == 1:
-                    airports.append(match[0])
-                elif len(match) == 0:
-                    pass
-                else:
-                    # Problems...
-                    airports.append(match[0])
-            elif len(code) == 4:
-                match = Airport.objects.filter(icao=code.upper())
-                if len(match) == 1:
-                    airports.append(match[0])
-                elif len(match) == 0:
-                    pass
-                else:
-                    # Problems...
-                    airports.append(match[0])
-            else:
-                # This should never happen
-                raise ValueError('Invalid POST data')
-        
-        routes = []
-        for i in range(len(airports)-1):
-            routes.append({'origin': airports[i], 
-                           'destination': airports[i+1], 
-                           'distance': int(airports[i].distance_to(airports[i+1]))
-                          })
-        
-        context = {'method': 'POST',
-                   'nav_id': 'gcmap_nav',
-                   'routes': routes,
-                   'airports': airports,
-                   'map_center_lat': request.POST['map_center_lat'],
-                   'map_center_lon': request.POST['map_center_lon'],
-                   'map_zoom': request.POST['map_zoom'],
-                  }
-        
-        return render(request, 'flights/gcmap.html', context)
-    else:
-        origin = Airport.objects.filter(iata='DFW')[0]
-        
-        context = {'method': 'GET',
-                   'nav_id': 'gcmap_nav',
-                  }
-        
-        return render(request, 'flights/gcmap.html', context)
-        
+    except:
+        raise ValueError('Invalid AJAX data')
 
+    # Look up codes
+    airports = []
+    for code in codes:
+        if len(code) == 3:
+            match = Airport.objects.filter(iata=code.upper())
+            if len(match) == 1:
+                airports.append(match[0])
+            elif len(match) == 0:
+                pass
+            else:
+                # Problems...
+                airports.append(match[0])
+        elif len(code) == 4:
+            match = Airport.objects.filter(icao=code.upper())
+            if len(match) == 1:
+                airports.append(match[0])
+            elif len(match) == 0:
+                pass
+            else:
+                # Problems...
+                airports.append(match[0])
+        else:
+            # This should never happen
+            raise ValueError('Invalid AJAX data')
+
+    airports_as_json = serializers.serialize('json', airports)
+
+    return JsonResponse(airports_as_json, safe=False)
 
 def airports(request):
     pass
