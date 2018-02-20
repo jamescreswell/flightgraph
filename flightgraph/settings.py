35873b1bb7e6dc14614290a968474080ee4fd70a
jamescreswell <jamescreswell4@gmail.com>
2018-02-20 20:22:51 +0100

    Add hostname www.flightgraph.eu

--- flightgraph/settings.py
+++ flightgraph/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '837g)bo)=)os%c-!*g*ukav%8ykpjlld*kjvkk+d^2hzk-@##n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '54.91.110.22', '54.234.162.159', '54.197.73.8']
+ALLOWED_HOSTS = ['127.0.0.1', '54.91.110.22', '54.234.162.159', '54.197.73.8', 'www.flightgraph.eu']
 
 
 # Application definition
